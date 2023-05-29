5a0f3268d038a4ec7d98c31e21478226b35bda12
adamek645 <adamek645@gmail.com>
2023-05-29 18:06:18 +0200

    Poprawki Main, Skrzyzowanie, Droga, Slot, nie wiem czy jeszcze dobrze, sprawdzi się

--- src/Droga.java
+++ src/Droga.java
@@ -7,7 +7,6 @@ public class Droga {
     private int pozx;
     private int pozy;
     private int rotate;
-    Slot slot = new Slot();
     Droga(int id, int pozx, int pozy, int rotate) {
         this.id = id;
         this.pozx = pozx;
