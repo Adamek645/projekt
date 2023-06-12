470e1e2d736f4455f59802e354175d8d8e8c10ae
CringeCorchak <Michael_rychle@wp.pl>
2023-06-12 20:41:33 +0200

    Dodano cały przejazd pojazdów w trakcie trwania jednego cyklu próbnego wraz ze względnymi parametrami wyników

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.List;
-
+import java.util.Random;
 public class Main {
     public static void main(String[] args) throws FileNotFoundException {
         List<Droga> drogi = new ArrayList<>();
@@ -11,27 +11,38 @@ public class Main {
         Sciezka.dodanieSciezki(skrzyzowanie, sciezki);
         /* Symulacja symulacja = new Symulacja();
         Symulacja.symulacja(skrzyzowanie); */
-        int czasSwiatel = 60;
+        int czasSwiatel = 10;
         int [][] scierzka = { {1, 1, 1, 1,1,1,1}, {1, 2, 3, 4,5,6,7} };
-        int m,x,y;
+        int m,x,y,time = czasSwiatel;
+        Random rand = new Random();
         Map mapa = new Map(60,60);
-         //   while(czasSwiatel > 0 || Map.mapContents()){
+            while(czasSwiatel > 0 || Map.mapContents()){
+                time = time + 1;
                 x = scierzka[0][0];
                 y = scierzka[0][1];
-                if(mapa.getMap(x,y) == null && czasSwiatel > 0)
-                    Symulacja.generowaniePojazdu(scierzka);
-                    mapa.setMap(Pojazd.List.get(Pojazd.List.size()-1).x,Pojazd.List.get(Pojazd.List.size()-1).y,Pojazd.List.get(Pojazd.List.size()-1));
+                if(rand.nextInt(100) < 40)
+                    if(mapa.getMap(x,y) == null && czasSwiatel > 0) {
+                        Symulacja.generowaniePojazdu(scierzka);
+                        mapa.setMap(Pojazd.List.get(Pojazd.List.size() - 1).x, Pojazd.List.get(Pojazd.List.size() - 1).y, Pojazd.List.get(Pojazd.List.size() - 1));
+                     }
                 for(int l = 0; l < Pojazd.List.size(); l++) {
                     m = Pojazd.List.get(l).vmax;
                     while (m > 0){
-                        Pojazd.List.get(l).ruchPojazdu();
+                        if((Pojazd.List.get(l).C + 1) == Pojazd.List.get(l).droga.length) {
+                            mapa.setMap(Pojazd.List.get(l).x,Pojazd.List.get(l).y,null);
+                            m=0;
+                        }
+                        else Pojazd.List.get(l).ruchPojazdu();
                         m--;
                     }
                 }
                 czasSwiatel--;
-          //  }
-
+            }
+        System.out.println(Pojazd.List);
             Pojazd.clearList();
-
+        System.out.println(Symulacja.sumaPkt);
+        System.out.println(Symulacja.count);
+        System.out.println((float)Symulacja.count / time);
+        System.out.println(Symulacja.zliczanie(Symulacja.sumaV, time));
     }
 }
--- src/Symulacja.java
+++ src/Symulacja.java
@@ -2,6 +2,8 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Random;
 public class Symulacja {
+    static double sumaPkt = 0;
+    static int sumaV = 0, count = 0;
   /*  static ZapisPliku save;
      static {
         try {
@@ -18,17 +20,25 @@ public class Symulacja {
         int los = random.nextInt(10);
         if(los < 4) {
             Pojazd.List.add(new Samochod((random.nextInt(2) + 3), scierzka));
+            sumaPkt = sumaPkt + 1;
         }
         else if(los < 6) {
             Pojazd.List.add(new Autobus((random.nextInt(2) + 1), scierzka));
+            sumaPkt = sumaPkt + 2;
         }
         else {
             Pojazd.List.add(new Ciezarowka((random.nextInt(2) + 2), scierzka));
+            sumaPkt = sumaPkt + 1.6;
         }
+        count = count + 1;
+        sumaV = sumaV + Pojazd.List.get(Pojazd.List.size()-1).vmax;
         return Pojazd.List.get(Pojazd.List.size()-1);
     }
-    public static double zliczanie(int dldrogi, int v){
-        return ((double)dldrogi/(double)v);
+    public static double zliczanie(int sumav, int time){
+        sumaV = 0;
+        count = 0;
+        sumaPkt = 0;
+        return ((double)sumav/(double)time);
     }
     public static void kontrolaKolizji(){}
 }
