18bd8f78f9ee5660722738e6a8c4c1503206150d
CringeCorchak <Michael_rychle@wp.pl>
2023-06-15 14:17:44 +0200

    poprawka prędkości

--- src/Symulacja.java
+++ src/Symulacja.java
@@ -1,30 +1,26 @@
-import java.io.IOException;
-import java.util.ArrayList;
 import java.util.Random;
 public class Symulacja {
     static double sumaPkt = 0;
     static int sumaV = 0, count = 0;
-    public static void symulacja(Skrzyzowanie skrzyzowanie){
+
+    public static void symulacja(Skrzyzowanie skrzyzowanie) {
     }
 
-    public static Pojazd generowaniePojazdu(int sciezka[][]){
+    public static Pojazd generowaniePojazdu(int sciezka[][]) {
         Random random = new Random();
         int los = random.nextInt(10);
-        if(los < 4) {
-            Pojazd.List.add(new Samochod((random.nextInt(2) + 3), sciezka));
+        if (los < 4) {
+            Pojazd.List.add(new Samochod((random.nextInt(6) + 3), sciezka));
             sumaPkt = sumaPkt + 1;
-        }
-        else if(los < 6) {
-            Pojazd.List.add(new Autobus((random.nextInt(2) + 1), sciezka));
+        } else if (los < 6) {
+            Pojazd.List.add(new Autobus((random.nextInt(3) + 1), sciezka));
             sumaPkt = sumaPkt + 2;
-        }
-        else {
-            Pojazd.List.add(new Ciezarowka((random.nextInt(2) + 2), sciezka));
+        } else {
+            Pojazd.List.add(new Ciezarowka((random.nextInt(3) + 2), sciezka));
             sumaPkt = sumaPkt + 1.6;
         }
         count = count + 1;
-        sumaV = sumaV + Pojazd.List.get(Pojazd.List.size()-1).vmax;
-        return Pojazd.List.get(Pojazd.List.size()-1);
+        sumaV = sumaV + Pojazd.List.get(Pojazd.List.size() - 1).vmax;
+        return Pojazd.List.get(Pojazd.List.size() - 1);
     }
-    public static void kontrolaKolizji(){}
 }
