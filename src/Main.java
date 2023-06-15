6dee19a5686b43f168f486d100cb82b4de8a4e96
adamek645 <adamek645@gmail.com>
2023-06-15 19:38:16 +0200

    Zmiany dosyć spore, bo -> pojazdy są wypuszczane przy pomocy minimalnego odstępu. Obliczenia E/cykl, odpowiedni vmax, adekwatny do rzeczywistosci. Ogólnie działa.

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,7 @@
 import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Random;
+import java.util.*;
+
 public class Main {
 
     public static void main(String[] args) throws FileNotFoundException {
@@ -20,25 +18,33 @@ public class Main {
         Sciezka.dodanieSciezki(skrzyzowanie, sciezki);
         PunktKolizji.wyznaczaniePunktow(punktyKolizji, sciezki);
         int[][] sciezka = Skrzyzowanie.getSciezka(0);
-        /* Symulacja symulacja = new Symulacja();
-        Symulacja.symulacja(skrzyzowanie); */
-        int czasSwiatel = 1200;
-        int m,x,y,a = 0,b = 1,time = czasSwiatel,pojazdlimit = 30, zasieg = 100;
+        Scanner in = new Scanner(System.in);
+        System.out.print("Wpisz długość wyświetlania zielonego światła dla jednego wejścia (w s, 30/40/50): ");
+        int czasSwiatelSt = in.nextInt()*10;
+        int liczbaCykli = 36000/(czasSwiatelSt*3);
+        int m,x,y,a = 0,b = 1,time = 0,timeC = 0, pojazdlimit = 50, zasieg = 100, pop = 0, pojPop = 0, sumaPkt = 0, count = 0, sumaPktMin = 100000, sumaPktMax = 0;
         Random rand = new Random();
         Map mapa = new Map(601,601);
-        for(int iteracja = 0; iteracja < 4; iteracja++){
+        for(int iteracja = 0; iteracja < liczbaCykli; iteracja++){
         for(int cykl = 0; cykl < 3;cykl++){
-            czasSwiatel = 1200;
-            time = czasSwiatel;
-            while (czasSwiatel > 0 || Map.mapContents()) {
+            int czasSwiatel = czasSwiatelSt;
+             while (czasSwiatel > 0 || Map.mapContents()) {
                 time = time + 1;
-                if (rand.nextInt(100) < 40 && pojazdlimit > 0 && czasSwiatel > 0) {
+                if (rand.nextInt(100) < 30 && pojazdlimit > 0 && czasSwiatel > 80 && (time-pop)*12>=pojPop) {
                     pojazdlimit--;
                     if (rand.nextInt(2) == 1) {
                         x = Skrzyzowanie.getSciezka(a)[0][0];
                         y = Skrzyzowanie.getSciezka(a)[0][1];
                         if (mapa.getMap(x, y) == null) {
                             Symulacja.generowaniePojazdu(Skrzyzowanie.getSciezka(a));
+                            pop = time;
+                            if(Pojazd.List.get(Pojazd.List.size()-1).vmax == 12) {
+                                pojPop = 230;
+                            } else if (Pojazd.List.get(Pojazd.List.size()-1).vmax == 13) {
+                                pojPop = 170;
+                            } else if (Pojazd.List.get(Pojazd.List.size()-1).vmax == 14) {
+                                pojPop = 110;
+                            }
                             mapa.setMap(Pojazd.List.get(Pojazd.List.size() - 1).x, Pojazd.List.get(Pojazd.List.size() - 1).y, Pojazd.List.get(Pojazd.List.size() - 1));
                         } else {
                             x = Skrzyzowanie.getSciezka(b)[0][0];
@@ -64,8 +70,11 @@ public class Main {
                         }
                         }
                 }
-                if (pojazdlimit <= 0 && Pojazd.List.size() > 0 && mapa.getMap(Pojazd.List.get(Pojazd.List.size() - 1).x, Pojazd.List.get(Pojazd.List.size() - 1).y) == null) {
+                if (czasSwiatel < 2 && Pojazd.List.size() > 0 && mapa.getMap(Pojazd.List.get(Pojazd.List.size() - 1).x, Pojazd.List.get(Pojazd.List.size() - 1).y) == null) {
                     mapa.mapClear();
+                    pojazdlimit = 50;
+                    pop = 0;
+                    pojPop = 0;
                     break;
                 }
                 czasSwiatel--;
@@ -74,19 +83,32 @@ public class Main {
             a += 2;
             b += 2;
         }
-            System.out.println("Suma Punktów: " + Symulacja.sumaPkt);
-            System.out.println("Ilość pojazdów w symulacji: " + Symulacja.count);
+            sumaPkt = sumaPkt + Symulacja.sumaPkt;
+            count = count + Symulacja.count;
+            timeC = timeC + time;
+            if(Symulacja.sumaPkt<sumaPktMin) {
+                sumaPktMin = Symulacja.sumaPkt;
+            }
+            if(Symulacja.sumaPkt>sumaPktMax) {
+                sumaPktMax = Symulacja.sumaPkt;
+            }
+            System.out.println("Wartość przepustowości E/cykl: " + (float)Symulacja.sumaPkt/100);
+            System.out.println("Liczba pojazdów w cyklu: " + Symulacja.count);
             System.out.println("Średnia prędkość: " + (double) Symulacja.sumaV / Symulacja.count);
-            System.out.println("Czas symulacji: " + time);
+            System.out.println("Czas symulacji cyklu w sekundach: " + time/10);
             ZapisPliku.zapis(Symulacja.count,Symulacja.sumaPkt,time);
             Symulacja.sumaPkt = 0;
             Symulacja.count = 0;
             Symulacja.sumaV = 0;
             time = 0;
-            pojazdlimit = 30;
+            pojazdlimit = 50;
             a = 0;
             b = 1;
         }
+        System.out.println("Wartość przepustowości E/h: " + sumaPkt/100);
+        System.out.println("Liczba pojazdów w ciagu godziny: " + count + "\n");
+        System.out.println("Minimalna przepustowość (E/cykl) w badaniu: " + (double)sumaPktMin/100);
+        System.out.println("Maksymalna przepustowość (E/cykl) w badaniu: " + (double)sumaPktMax/100);
     }
 }
 /*
--- src/Symulacja.java
+++ src/Symulacja.java
@@ -1,6 +1,6 @@
 import java.util.Random;
 public class Symulacja {
-    static double sumaPkt = 0;
+    static int sumaPkt = 0;
     static int sumaV = 0, count = 0;
 
     public static void symulacja(Skrzyzowanie skrzyzowanie) {
@@ -9,15 +9,15 @@ public class Symulacja {
     public static Pojazd generowaniePojazdu(int sciezka[][]) {
         Random random = new Random();
         int los = random.nextInt(10);
-        if (los < 4) {
-            Pojazd.List.add(new Samochod((random.nextInt(6) + 3), sciezka));
-            sumaPkt = sumaPkt + 1;
-        } else if (los < 6) {
-            Pojazd.List.add(new Autobus((random.nextInt(3) + 1), sciezka));
-            sumaPkt = sumaPkt + 2;
+        if (los < 8) {
+            Pojazd.List.add(new Samochod(14, sciezka));
+            sumaPkt = sumaPkt + 100;
+        } else if (los < 9) {
+            Pojazd.List.add(new Autobus(13, sciezka));
+            sumaPkt = sumaPkt + 200;
         } else {
-            Pojazd.List.add(new Ciezarowka((random.nextInt(3) + 2), sciezka));
-            sumaPkt = sumaPkt + 1.6;
+            Pojazd.List.add(new Ciezarowka(12, sciezka));
+            sumaPkt = sumaPkt + 160;
         }
         count = count + 1;
         sumaV = sumaV + Pojazd.List.get(Pojazd.List.size() - 1).vmax;
